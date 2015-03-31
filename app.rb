887aef274a71a1102fd67cbe116c5c96f18628c7
Daviddsr <daviddsrperiodismo@gmail.com>
2015-03-31 12:31:03 +0200

    indentation fixed

--- app.rb
+++ app.rb
@@ -7,6 +7,6 @@ end
 post '/upload' do
 	File.open('uploads/' + params['myfile'][:filename], "w") do |f|
     f.write(params['myfile'][:tempfile].read)
-	end
-return "The file was successfully uploaded!"
+  end
+  return "The file was successfully uploaded!"
 end
