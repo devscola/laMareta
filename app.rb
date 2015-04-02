41428dd602f78c1b96674e1f421b987464db336a
Javier Aristizabal <javieraristizabal@gmail.com>
2015-04-02 13:26:50 +0200

    Test jquery upload file

--- app.rb
+++ app.rb
@@ -1,4 +1,6 @@
 require 'sinatra'
+require 'roo'
+require 'csv'
 
 get '/' do
 	erb :upload
@@ -8,4 +10,22 @@ post '/upload' do
 	File.open('uploads/' + params['birthdayFile'][:filename], "w") do |f|
     f.write(params['birthdayFile'][:tempfile].read)
   end
+  excelFile = 'uploads/' + params['birthdayFile'][:filename]
+  csv_converter(excelFile)
+
+end
+
+def csv_converter (excelFile)
+  if excelFile =~ /xlsx$/
+    excel = Roo::Excelx.new(excelFile)
+  else
+    excel = Roo::Excel.new(excelFile)
+  end
+
+  output = File.open('uploads/test.csv', "w")
+
+2.upto(excel.last_row) do |line|
+  output.write CSV.generate_line excel.row(line)
+end
+
 end
