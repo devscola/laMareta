8cc40c3322ad35c5fdbdaa266f4943d0bb7b630b
Daviddsr <daviddsrperiodismo@gmail.com>
2015-04-12 18:58:41 +0200

    Overwrite database propperly

--- app.rb
+++ app.rb
@@ -5,8 +5,10 @@ require 'roo'
 require 'pony'
 
 require './helpers/code'
+require './helpers/check_birthday_users'
 
 include Code
+include CheckUsers
 
 DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/users_mareta.db")
 
@@ -32,10 +34,17 @@ DataMapper.finalize.auto_upgrade!
 
 
 get '/' do
+  if File::exists?( "users_mareta.db" ) 
+    @users = User.all
+    CheckUsers.check_users_mareta(@users)
+  end
   erb :upload
 end
 
 post '/upload' do
+  users = User.all
+  users.destroy
+  
   filename = 'uploads/' + params['birthdayFile'][:filename]
 
   File.open(filename, "w") do |f|
@@ -56,32 +65,28 @@ post '/upload' do
 
     @user = User.create(:name => name,:birthday => birthday, :email => email)
   end
-  array_users = User.all
-  array_users.each do |user|
-    user_birthday = user.birthday.to_s.split('-')
-    user_birthday.shift
-    user_birthday = user_birthday.join
-    date = Date.today.to_s.split('-')
-    date.shift
-    date = date.join
-    p user_birthday
-    p date
-    if user_birthday = date
-      Pony.mail({:to => user.email,
-            :from => "daviddsrperiodismo@gmail",
-            :subject => 'Happy Birthday¡¡',
-            :body => "Happy Birthday #{user.name}, you have a free meal with this code #{Code.generate}",
-            :via => :smtp,
-            :via_options => {
-              :address              => 'smtp.gmail.com',
-              :port                 => '587',
-              :enable_starttls_auto => true,
-              :user_name            => 'daviddsrperiodismo@gmail.com',
-              :password             => '20041990',
-              :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
-              :domain               => "localhost" # the HELO domain provided by the client to the server
-            }})
-    end
-  end
+    # user_birthday = user.birthday.to_s.split('-')
+    # user_birthday.shift
+    # user_birthday = user_birthday.join
+    # date = Date.today.to_s.split('-')
+    # date.shift
+    # date = date.join
+    # if user_birthday = date
+    #   Pony.mail({:to => user.email,
+    #         :from => "daviddsrperiodismo@gmail",
+    #         :subject => 'Happy Birthday¡¡',
+    #         :body => "Happy Birthday #{user.name}, you have a free meal with this code #{Code.generate}",
+    #         :via => :smtp,
+    #         :via_options => {
+    #           :address              => 'smtp.gmail.com',
+    #           :port                 => '587',
+    #           :enable_starttls_auto => true,
+    #           :user_name            => 'daviddsrperiodismo@gmail.com',
+    #           :password             => '20041990',
+    #           :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
+    #           :domain               => "localhost" # the HELO domain provided by the client to the server
+    #         }})
+    # end
+  
   redirect '/'
 end
---
+++ helpers/check_birthday_users.rb
@@ -0,0 +1,16 @@
+
+
+module CheckUsers
+
+  def check_users_mareta(users)
+    users.each do |user|
+      birthday_user = Time.parse(user.birthday.to_s)
+      birthday_user = Time.mktime(0, user.birthday.month, user.birthday.day)
+      date_today = Time.parse(Date.today.to_s)
+      date_today = Time.mktime(0, Date.today.month, Date.today.day)
+      p birthday_user
+      p date_today
+    end
+  end
+
+end
