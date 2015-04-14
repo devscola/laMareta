fe6c5e5ef40d6c77ad53489b30e7b6b771f68f68
Javier Aristizabal <javieraristizabal@gmail.com>
2015-04-14 12:02:38 +0200

    Testing mail

--- app.rb
+++ app.rb
@@ -68,19 +68,19 @@ post '/upload' do
     p date
     if user_birthday = date
       Pony.mail({:to => user.email,
-            :from => "daviddsrperiodismo@gmail",
-            :subject => 'Happy Birthday¡¡',
-            :body => "Happy Birthday #{user.name}, you have a free meal with this code #{Code.generate}",
-            :via => :smtp,
-            :via_options => {
-              :address              => 'smtp.gmail.com',
-              :port                 => '587',
-              :enable_starttls_auto => true,
-              :user_name            => 'daviddsrperiodismo@gmail.com',
-              :password             => '20041990',
+        :from => "daviddsrperiodismo@gmail",
+        :subject => 'Happy Birthday¡¡',
+        :body => "Happy Birthday #{user.name}, you have a free meal with this code #{Code.generate}",
+        :via => :smtp,
+        :via_options => {
+          :address              => 'smtp.gmail.com',
+          :port                 => '587',
+          :enable_starttls_auto => true,
+          :user_name            => 'daviddsrperiodismo@gmail.com',
+          :password             => '20041990',
               :authentication       => :plain, # :plain, :login, :cram_md5, no auth by default
               :domain               => "localhost" # the HELO domain provided by the client to the server
-            }})
+              }})
     end
   end
   redirect '/'
