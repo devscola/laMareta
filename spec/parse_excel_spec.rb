0922642dc0573130e7f4e8870b72ef54254979f3
Daviddsr <daviddsrperiodismo@gmail.com>
2015-05-21 16:38:38 +0200

    Refactor tests

--- spec/parse_excel_spec.rb
+++ spec/parse_excel_spec.rb
@@ -33,32 +33,4 @@ describe ExcelParser do
       expect{ExcelParser.parse(file_path)}.to raise_error(NameError)
     end
   end
-  # describe "#validates_format" do
-  #   it "validates the dates format in excel file" do
-  #     file_path = File.join(File.dirname(__FILE__), 'fixtures', 'databasetest.xlsx')
-  #     excel_file = ExcelParser.create_roo_object(file_path)
-  #     expect{validates_format(excel_file)}.not_to raise_error
-  #   end
-  #   context "#valid_email? wrong" do
-  #     it "raises an error when excel email format is not correct" do
-  #       file_path = File.join(File.dirname(__FILE__), 'fixtures', 'databasetest_bad_email.xlsx')
-  #       excel_file = ExcelParser.create_roo_object(file_path)
-  #       expect{validates_format(excel_file)}.to raise_error(EmailError)
-  #     end
-  #   end
-  #   context "#valid_name? wrong" do
-  #     it "raises an error when excel name format is not correct" do
-  #       file_path = File.join(File.dirname(__FILE__), 'fixtures', 'databasetest_bad_name.xlsx')
-  #       excel_file = ExcelParser.create_roo_object(file_path)
-  #       expect{validates_format(excel_file)}.to raise_error(NameError)
-  #     end
-  #   end
-  #   context "#valid_date? wrong" do
-  #     it "raises an error when excel date format is not correct" do
-  #       file_path = File.join(File.dirname(__FILE__), 'fixtures', 'databasetest_bad_date.xlsx')
-  #       excel_file = ExcelParser.create_roo_object(file_path)
-  #       expect{validates_format(excel_file)}.to raise_error(DateFormatError)    
-  #     end
-  #   end
-  # end
 end
