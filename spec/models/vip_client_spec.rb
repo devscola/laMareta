fb441f05b66f2360573c32aed70aec4d0de32da8
Daviddsr <daviddsrperiodismo@gmail.com>
2015-05-21 16:51:46 +0200

    Refactor tests vip_client_spec.rb

--- spec/models/vip_client_spec.rb
+++ spec/models/vip_client_spec.rb
@@ -27,22 +27,6 @@ describe VipClient do
       VipClient.insert_into_database(list_clients)
       expect(VipClient.all.count).to eq(2)
     end
-  end
-  describe "#exists?" do
-    it "returns true if the vip client already exists in database" do
-      list_clients = [
-        {name: "David", birthday: "13-12-1985", email: "daviddsrperiodismo@gmail.com"},
-        {name: "Javier", birthday: "05-05-1985", email: "javier@gmail.com"}
-      ]
-      list_clients2 = [
-        {name: "David", birthday: "13-12-1985", email: "daviddsrperiodismo@gmail.com"},
-        {name: "Javier", birthday: "05-05-1985", email: "javier@gmail.com"},
-        {name: "Paco", birthday: "02-08-1985", email: "pacofiestas@gmail.com"}
-      ]
-      VipClient.insert_into_database(list_clients)
-      client = list_clients2[0]
-      expect(VipClient.exists?(client)).to eq(true)
-    end
     it "updates the database with the new vip clients except the vip clients that already exists" do
       list_clients = [
         {name: "David", birthday: "13-12-1985", email: "daviddsrperiodismo@gmail.com"},
@@ -56,18 +40,6 @@ describe VipClient do
       VipClient.insert_into_database(list_clients)
       VipClient.insert_into_database(list_clients2)
       expect(VipClient.all.count).to eq(3)
-    end
-  end
-  describe "VipClient properties" do
-    it "a client in database has 3 propperties: name, email and birthday" do
-      list_clients = [
-        {name: "David", birthday: "13-12-1985", email: "daviddsrperiodismo@gmail.com"}
-      ]
-      VipClient.insert_into_database(list_clients)
-      client = VipClient.first
-      expect(client.name).to eq("David")
-      expect(client.birthday.to_s).to eq("1985-12-13")
-      expect(client.email).to eq("daviddsrperiodismo@gmail.com")      
     end
   end
 end
